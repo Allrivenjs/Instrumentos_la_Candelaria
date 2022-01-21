ea7c70ac2c020628550804abba2c2a6127593da5
fabiloco <faalsaru@gmail.com>
2022-01-21 01:39:46 -0500

    products page

--- frontend/instrumentos_la_candelaria/src/services.js
+++
@@ -1,9 +0,0 @@
-export const getProducts = async (array) => {
-    try {
-        const res = await fetch(`http://127.0.0.1:8000/api/products`);
-        const data = await res.json();
-        return data;
-    } catch(e) {
-        console.log(e);
-    };
-};
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html", "./product.html", "./shopping_cart.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
